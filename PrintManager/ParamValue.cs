f19778847e69969169d80e9c51fa61758c265a4d
Enrique Alonso Rivera Nevarez <e.a_wf@hotmail.com>
2014-02-05 01:04:52 -0700

    Test

--- PrintManager/ParamValue.cs
+++ PrintManager/ParamValue.cs
@@ -3,40 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PrintManager
+/*
+* Ejemplo:
+* Query = SELECT HojasDisponibles FROM Users WHERE idUser = @idUser
+* Param deberá contener la cadena "idUser"
+* Value deberá contener el valor que reemplazará el parámetro indicado (@idUser).
+* para poder realizar la consulta.
+* 
+* Entonces, Param = "idUser" y Value = "09550563"
+* Al hacer la consulta quedaría de la siguiente forma:
+* SELECT HojasDisponibles FROM Users WHERE idUser = 09550563
+*/
+
+/// <summary>
+/// Clase que encapsula el parámetro y valor para cada consulta parametrizada.
+/// </summary>
+class ParamValue
 {
-    /*
-    * Ejemplo:
-    * Query = SELECT HojasDisponibles FROM Users WHERE idUser = @idUser
-    * Param deberá contener la cadena "idUser"
-    * Value deberá contener el valor que reemplazará el parámetro indicado (@idUser).
-    * para poder realizar la consulta.
-    * 
-    * Entonces, Param = "idUser" y Value = "09550563"
-    * Al hacer la consulta quedaría de la siguiente forma:
-    * SELECT HojasDisponibles FROM Users WHERE idUser = 09550563
-    */
+    #region Instance variables
 
-    /// <summary>
-    /// Clase que encapsula el parámetro y valor para cada consulta parametrizada.
-    /// </summary>
-    class ParamValue
+    public string Param { get; set; }
+    public string Value { get; set; }
+
+    #endregion
+
+    #region Constructor
+
+    public ParamValue(string param, string value)
     {
-        #region Instance variables
-
-        public string Param { get; set; }
-        public string Value { get; set; }
-
-        #endregion
-
-        #region Constructor
-
-        public ParamValue(string param, string value)
-        {
-            Param = param;
-            Value = value;
-        }
-
-        #endregion
+        Param = param;
+        Value = value;
     }
+
+    #endregion
 }
