f038689054c6c4474b6f58ad5832a6f54c6c615f
Enrique Alonso Rivera Nevarez <e.a_wf@hotmail.com>
2014-02-05 00:52:25 -0700

    Modifier 'Internal' eliminated

--- PrintManager/Query.cs
+++ PrintManager/Query.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace PrintManager
+/// <summary>
+/// Clase que almacena las querys.
+/// </summary>
+public class Query
 {
-    /// <summary>
-    /// Clase que almacena las querys.
-    /// </summary>
-    class Query
-    {
-        #region QueryStrings
+    #region QueryStrings
+        
+    public static string AvailableSheets = "SELECT HojasDisponibles FROM Users WHERE idUser = @idUser";
+    public static string UserInfo = "SELECT Nombre_1, Nombre_2, Apellido_1, Apellido_2, Carrera, HojasDisponibles  FROM Users WHERE idUser = @idUser";
+    public static string CheckPassword = "SELECT idUser FROM Users WHERE idUser = @idUser AND password = @password";
+    public static string UpdateAvailableSheets = "UPDATE users SET HojasDisponibles = @Sheets WHERE idUser = @idUser";
+    public static string InsertHistorial = "INSERT INTO historialtrabajos (nombreArchivo, numPaginas, fecha, idUser) VALUES (@jobName, @pages, @date, @idUser)";
 
-        internal static string AvailableSheets = "SELECT HojasDisponibles FROM Users WHERE idUser = @idUser";
-        internal static string UserInfo = "SELECT Nombre_1, Nombre_2, Apellido_1, Apellido_2, Carrera, HojasDisponibles  FROM Users WHERE idUser = @idUser";
-        internal static string CheckPassword = "SELECT idUser FROM Users WHERE idUser = @idUser AND password = @password";
-        internal static string UpdateAvailableSheets = "UPDATE users SET HojasDisponibles = @Sheets WHERE idUser = @idUser";
-        internal static string InsertHistorial = "INSERT INTO historialtrabajos (nombreArchivo, numPaginas, fecha, idUser) VALUES (@jobName, @pages, @date, @idUser)";
-
-        #endregion
-    }
+    #endregion
 }
